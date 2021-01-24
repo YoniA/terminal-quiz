32ebeeeded365d4b6ede0c5d9b28e04f474149b0
YoniA <YoniA@cat.com>
2021-01-24 21:42:20 +0200

    create tables for items, keys, domains, stats and items_domains

--- quiz_runner.sh
+++ quiz_runner.sh
@@ -2,6 +2,11 @@
 
 
 SEPARATOR="\n\n"
+RED='\e[31m'
+GREEN='\e[32m'
+BLINK='\e[5m'
+
+
 stem=$(sqlite3 quiz.db "select stem from items where item_id=1")
 ans1=$(sqlite3 quiz.db "select ans1 from items where item_id=1")
 ans2=$(sqlite3 quiz.db "select ans2 from items where item_id=1")
@@ -11,15 +16,20 @@ ans4=$(sqlite3 quiz.db "select ans4 from items where item_id=1")
 
 
 print_item(){
-echo "$1"
-echo "\n\n"
-echo "a.\t $2"
-echo "b.\t $3"
-echo "c.\t $4"
-echo "d.\t $5"	
+	echo "$1"
+	echo $SEPARATOR
+	echo "a.\t$2"
+	echo "b.\t$3"
+	echo "c.\t$4"
+	echo "d.\t$5"
+	check_correctness	
 }
 
 check_correctness() {
+	# todo
+}
+
+is_mastered() {
 	# todo
 }
 
@@ -28,5 +38,8 @@ print_item $stem $ans1 $ans2 $ans3 $ans4
 echo $SEPARATOR
 
 # zsh equivalent to 'read -p' in bash
-vared -p 'answer: ' -c response
-echo your response is: $response
+vared -p "answer: " -c response
+echo -e "your response is: ${GREEN}correct!"
+
+
+# colors: https://misc.flogisoft.com/bash/tip_colors_and_formatting
