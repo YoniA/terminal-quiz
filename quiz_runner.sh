74fdf16ac872d6adefbf869b44c66058d1793a78
YoniA <YoniA@cat.com>
2021-01-24 23:02:00 +0200

    implement few methods and choose random item to show

--- quiz_runner.sh
+++ quiz_runner.sh
@@ -5,13 +5,18 @@ SEPARATOR="\n\n"
 RED='\e[31m'
 GREEN='\e[32m'
 BLINK='\e[5m'
+MASTERED_THRESHOLD=5
 
+# select random item
+num_records=$(sqlite3 quiz.db "select count(*) from items");
+iid=$(shuf -i 1-$num_records -n 1)   
+echo $iid
 
-stem=$(sqlite3 quiz.db "select stem from items where item_id=1")
-ans1=$(sqlite3 quiz.db "select ans1 from items where item_id=1")
-ans2=$(sqlite3 quiz.db "select ans2 from items where item_id=1")
-ans3=$(sqlite3 quiz.db "select ans3 from items where item_id=1")
-ans4=$(sqlite3 quiz.db "select ans4 from items where item_id=1")
+stem=$(sqlite3 quiz.db "select stem from items where iid=${iid}")
+ans1=$(sqlite3 quiz.db "select ans1 from items where iid=${iid}")
+ans2=$(sqlite3 quiz.db "select ans2 from items where iid=${iid}")
+ans3=$(sqlite3 quiz.db "select ans3 from items where iid=${iid}")
+ans4=$(sqlite3 quiz.db "select ans4 from items where iid=${iid}")
 
 
 
@@ -21,16 +26,25 @@ print_item(){
 	echo "a.\t$2"
 	echo "b.\t$3"
 	echo "c.\t$4"
-	echo "d.\t$5"
-	check_correctness	
+	echo "d.\t$5"	
 }
 
 check_correctness() {
-	# todo
+	key=$(sqlite3 quiz.db "select key from keys where iid=$1")
+
+	# echo "key is $key"
+	# echo "response is $2"
+	if [[ "$key" == "$2" ]]; then
+		echo -e "your response $2 is ${GREEN}correct!"
+	else
+		echo -e "your response $2 is ${RED}wrong."
+	fi
 }
 
+
 is_mastered() {
-	# todo
+	mastered=$(sqlite3 quiz.db "select mastered from stats where iid=$1")
+	echo $mastered
 }
 
 
@@ -39,7 +53,9 @@ echo $SEPARATOR
 
 # zsh equivalent to 'read -p' in bash
 vared -p "answer: " -c response
-echo -e "your response is: ${GREEN}correct!"
+check_correctness $iid $response
+is_mastered $iid
+#echo -e "your response is: ${GREEN}correct!"
 
 
 # colors: https://misc.flogisoft.com/bash/tip_colors_and_formatting
