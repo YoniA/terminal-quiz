551b3f703af39eca0649d9d0cad453593b05a700
YoniA <YoniA@cat.com>
2021-02-02 22:17:24 +0200

    add more questions to the database, add stats implementation

--- quiz_runner.sh
+++ quiz_runner.sh
@@ -5,12 +5,11 @@ SEPARATOR="\n\n"
 RED='\e[31m'
 GREEN='\e[32m'
 BLINK='\e[5m'
-MASTERED_THRESHOLD=5
+MASTERED_THRESHOLD=3
 
 # select random item
 num_records=$(sqlite3 quiz.db "select count(*) from items");
 iid=$(shuf -i 1-$num_records -n 1)   
-echo $iid
 
 stem=$(sqlite3 quiz.db "select stem from items where iid=${iid}")
 ans1=$(sqlite3 quiz.db "select ans1 from items where iid=${iid}")
@@ -19,8 +18,14 @@ ans3=$(sqlite3 quiz.db "select ans3 from items where iid=${iid}")
 ans4=$(sqlite3 quiz.db "select ans4 from items where iid=${iid}")
 
 
+print_title() {
+	topic=$(sqlite3 quiz.db "select title from domains natural join items_domains where iid=$1")
+	title="Topic: $topic"
+	echo "$title"
+	printf "%0.s-" {1..${#title}} 	
+}
 
-print_item(){
+print_item() {
 	echo "$1"
 	echo $SEPARATOR
 	echo "a.\t$2"
@@ -41,12 +46,47 @@ check_correctness() {
 	fi
 }
 
+update_stats() {
+	attempts=$(sqlite3 quiz.db "select attempts from stats where iid=$1")
+	((attempts++))
+	
+	rights=$(sqlite3 quiz.db "select rights from stats where iid=$1")
+	streak=$(sqlite3 quiz.db "select streak from stats where iid=$1")
+	mastered=0;
+
+
+	key=$(sqlite3 quiz.db "select key from keys where iid=$1")
+	if [[ "$key" == "$2" ]]; then
+		((rights++))
+		((streak++))
+		
+		if [[ "$streak" == "$MASTERED_THRESHOLD" ]]; then
+			((mastered++))
+		fi
+	else
+		zero=0
+		streak=$zero
+		mastered=$zero
+	fi
+
+
+	
+	
+	sqlite3 quiz.db "update stats set attempts=${attempts}, rights=${rights}, streak=${streak}, mastered=$mastered where iid=$1"
+	echo "stats: attempts: ${attempts}, rights: ${rights}, streak=${streak}, mastered=${mastered}"	
+}
+
 
 is_mastered() {
 	mastered=$(sqlite3 quiz.db "select mastered from stats where iid=$1")
-	echo $mastered
+	if [[ "$mastered" == 1 ]]; then
+		echo "${BLINK}CONGRATULATIONS! You have mastered this question!"
+	fi
 }
 
+
+print_title $iid
+echo "\n"
 
 print_item $stem $ans1 $ans2 $ans3 $ans4
 echo $SEPARATOR
@@ -54,8 +94,10 @@ echo $SEPARATOR
 # zsh equivalent to 'read -p' in bash
 vared -p "answer: " -c response
 check_correctness $iid $response
-is_mastered $iid
-#echo -e "your response is: ${GREEN}correct!"
+#is_mastered $iid
 
+update_stats $iid $response
+
+is_mastered $iid
 
 # colors: https://misc.flogisoft.com/bash/tip_colors_and_formatting
