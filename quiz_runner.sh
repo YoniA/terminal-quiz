47591117bd368c139b988cd5009a2347445db7d6
YoniA <YoniA@cat.com>
2021-02-02 23:35:19 +0200

    add continue quiz functionality

--- quiz_runner.sh
+++ quiz_runner.sh
@@ -5,17 +5,46 @@ SEPARATOR="\n\n"
 RED='\e[31m'
 GREEN='\e[32m'
 BLINK='\e[5m'
-MASTERED_THRESHOLD=3
+MASTERED_THRESHOLD=5
 
-# select random item
-num_records=$(sqlite3 quiz.db "select count(*) from items");
-iid=$(shuf -i 1-$num_records -n 1)   
 
-stem=$(sqlite3 quiz.db "select stem from items where iid=${iid}")
-ans1=$(sqlite3 quiz.db "select ans1 from items where iid=${iid}")
-ans2=$(sqlite3 quiz.db "select ans2 from items where iid=${iid}")
-ans3=$(sqlite3 quiz.db "select ans3 from items where iid=${iid}")
-ans4=$(sqlite3 quiz.db "select ans4 from items where iid=${iid}")
+
+show_random_item() {
+	# num_records=$(sqlite3 quiz.db "select count(*) from items");
+	# iid=$(shuf -i 1-$num_records -n 1)
+
+	# show only items that are not mastered yet
+	iid=$(sqlite3 quiz.db "select iid from stats where mastered=0 order by random() limit 1")
+	
+	stem=$(sqlite3 quiz.db "select stem from items where iid=${iid}")
+	ans1=$(sqlite3 quiz.db "select ans1 from items where iid=${iid}")
+	ans2=$(sqlite3 quiz.db "select ans2 from items where iid=${iid}")
+	ans3=$(sqlite3 quiz.db "select ans3 from items where iid=${iid}")
+	ans4=$(sqlite3 quiz.db "select ans4 from items where iid=${iid}")
+
+	echo "\n"
+	print_title $iid
+	echo "\n"
+	
+	print_item $stem $ans1 $ans2 $ans3 $ans4
+	echo $SEPARATOR
+	
+	check_response
+	update_stats $iid $response	
+	is_mastered $iid
+
+	vared -p "show another question? enter y to continue; any other key to exit: " -c choice
+	case $choice in
+		y) 
+			show_random_item
+			;;
+			
+		*) 
+			exit
+			;;
+	esac
+}
+
 
 
 print_title() {
@@ -74,16 +103,9 @@ update_stats() {
 is_mastered() {
 	mastered=$(sqlite3 quiz.db "select mastered from stats where iid=$1")
 	if [[ "$mastered" == 1 ]]; then
-		echo "${BLINK}CONGRATULATIONS! You have mastered this question!"
+		echo "${BLINK}CONGRATULATIONS! You have mastered this question! it will not show again."
 	fi
 }
-
-
-print_title $iid
-echo "\n"
-
-print_item $stem $ans1 $ans2 $ans3 $ans4
-echo $SEPARATOR
 
 
 check_response() {
@@ -102,10 +124,7 @@ check_response() {
 }
 
 
-check_response
+show_random_item
 
-update_stats $iid $response
-
-is_mastered $iid
 
 # colors: https://misc.flogisoft.com/bash/tip_colors_and_formatting
