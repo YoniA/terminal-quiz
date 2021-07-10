c9f1ca8fd06449c68f9fc8a9ce1b938153241ae5
YoniA <YoniA@cat.com>
2021-07-10 19:57:49 +0300

    add more questions, add total questions to db_overview.sh

--- utils/db_overview.sh
+++ utils/db_overview.sh
@@ -2,3 +2,6 @@
 
 echo "Question distribution in the database:\n"
 sqlite3 quiz.db -header -column "select title as topic, count(did) as total from domains natural join items_domains group by did"
+
+echo "\n"
+echo "Total questions in the database:" $(sqlite3 quiz.db "select count(*) from items") 
