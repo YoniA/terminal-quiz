73a4752347eeed51fa2a8d1f721a6c0349ac9928
YoniA <YoniA@cat.com>
2022-02-09 21:33:53 +0200

    Wed 09 Feb 2022 21:33:44 IST

--- utils/dump_to_txt.sh
+++ utils/dump_to_txt.sh
@@ -2,4 +2,4 @@
 
 db=${1:-quiz.db}
 
-sqlite3 $db -line .output  "select * from items" > dump_$(date +'%F_%T').txt
+sqlite3 $db -line .output  "select iid, title, stem, ans1, ans2, ans3, ans4, key from items natural join keys natural join items_domains natural join domains" > dump_$(date +'%F_%T').txt
